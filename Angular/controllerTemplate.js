5d7d8ccfdd70c7165a657ce9881ae81f55de1648
andrewomacdonald <andrew.o.macdonald@gmail.com>
2017-08-19 13:11:29 -0700

    edited controllerTemplate and factoryTemplate

--- Angular/controllerTemplate.js
+++ Angular/controllerTemplate.js
@@ -3,8 +3,8 @@
     angular
         .module('NAME_OF_MODULE')
         .controller('NAME_OF_CONTROLLER', NAME_OF_CONTROLLER);
-
-    function NAME_OF_CONTROLLER($scope, 'WHATEVER ELSE YOU WANT TO INJECT') {
+    
+    function NAME_OF_CONTROLLER($scope, 'WHATEVER ELSE YOU WANT TO INJECT', 'NAME_OF FACTORY IF YOU INJECT A FACTORY') {
         // PERFORM ALL YOUR ANGULAR MAGIC HERE
 
     }
---
+++ Angular/factoryTemplate.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    angular
+        .module('NAME_OF_MODULE')
+        .factory('NAME_OF_FACTORY', NAME_OF_FACTORY);
+
+    var factory = {};
+
+    function NAME_OF_FACTORY(ANGULAR_MODULES_YOU_WANT_INJECTED, FOR_EXAMPLE:, $http) {
+        factory.nameOfFactoryFunction = function () {
+            //functionality here
+        };
+
+        factory.exampleOfUsing$http = function () {
+            return $http.get('/NAME_OF_ROUTE_HERE').then(function(data) {
+                return data;
+            });
+        }
+
+        return factory;
+    }
+
+})();
+
+//Generally used to abstract functionality that you'll be using across multiple controllers. if you want to call
+//a function from this factory, you would inject it into a controller and then invoke is as NAME_OF_FACTORY.nameOfFactoryFunction()
+
